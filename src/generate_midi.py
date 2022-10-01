6d142683c412069861b0dd3c404ccce5120c8cf9
lapptomi <lappetelainen.tomi@gmail.com>
2022-10-01 23:45:34 +0300

    added functionality to generate sequences of n length

--- src/generate_midi.py
+++ src/generate_midi.py
@@ -1,95 +1,58 @@
-import librosa
-import numpy as np
-from mido import MidiFile
-import mido
-from midiutil.MidiFile import MIDIFile
 from random import randrange
+from midiutil.MidiFile import MIDIFile
 
+# create your MIDI object
+mf = MIDIFile(1)     # only 1 track
+track = 0   # the only track
 
-# Track data
-data = MidiFile('test.mid')
-# data = MidiFile('bad1.mid')
+time = 0    # start at the beginning
+mf.addTrackName(track, time, 'Sample Track')
+mf.addTempo(track, time, 120)
 
-# Default bpm to 150 if tempo does not exist in the midi file
-bpm = mido.tempo2bpm(data.tracks[0][0].tempo) if hasattr(data.tracks[0][0], 'tempo') else 150
+# add some notes
 channel = 0
 volume = 100
 
-# Create your MIDI object
-mf = MIDIFile(1)     # only 1 track
-midi_track = 0   # the only track
+pitch = 52           # C4 (middle C)
+time = 8             # start on beat 0
+duration = 1         # 1 beat long
+mf.addNote(track, channel, pitch, time, duration, volume)
 
-time = 0    # start at the beginning
-mf.addTrackName(midi_track, time, "Sample Track")
-mf.addTempo(midi_track, time, bpm)
 
-"""
-Example note:
 pitch = 60           # C4 (middle C)
 time = 0             # start on beat 0
 duration = 1         # 1 beat long
 mf.addNote(track, channel, pitch, time, duration, volume)
-"""
 
-"""
-Create 127 * 127 adjacency matrix
-since there are 127 different MIDI note numbers.
+pitch = 64           # E4
+time = 2             # start on beat 2
+duration = 1         # 1 beat long
+mf.addNote(track, channel, pitch, time, duration, volume)
 
-For example MIDI note number 28 equals piano key E1 and 41 key F2 etc.
-Read more here:
-inspiredacoustics.com/en/MIDI_note_numbers_and_center_frequencies
-"""
-midi_notes_matrix = matrix = np.zeros((127, 127), dtype=int)
-starting_note = None
+pitch = 67           # G4
+time = 4             # start on beat 4
+duration = 1         # 1 beat long
+mf.addNote(track, channel, pitch, time, duration, volume)
 
-# Insert MIDI changes to the adjacency matrix
-for i, track in enumerate(data.tracks):
-    # print(f'Track ${i}: ${track.name}')
-    note_from = None
-    for message in track:
-        # print(message)
-        if message.type == 'note_on':
-            if not starting_note:
-                starting_note = message.note
-            # note_from is the current note that we are
-            # moving from to the next note
-            if not note_from:
-                note_from = message.note
-            midi_notes_matrix[note_from, message.note] += 1
-            note_from = message.note
-
-            
-current_note = starting_note
-next_note = None
-
-notes = [librosa.midi_to_note(current_note)]
-
-for row in range(127):
-    for col in range(127):
-        if midi_notes_matrix[row, col] > 0:
-            next_note = col
-            notes.append(librosa.midi_to_note(next_note))
-            next_note = col
-            pass
+pitch = 69           # A4
+time = 6             # start on beat 4
+duration = 1         # 1 beat long
+mf.addNote(track, channel, pitch, time, duration, volume)
 
 
-
-# Current beat, start on beat 0
-current_beat = 0
-
-# Add notes to the MIDI track
-for note in notes:
+# Add some random notes
+for i in range(6, 10):
+    print(i)
     mf.addNote(
-      track=midi_track,
+      track=track,
       channel=channel,
-      pitch=librosa.note_to_midi(note),
-      time=current_beat,
-      duration=randrange(2,4),
-      volume=randrange(70, 100)
+      pitch=randrange(30, 80),
+      time=randrange(i, i+4),
+      duration=randrange(1, 4),
+      volume=volume
     )
-    current_beat += randrange(0, 4)
 
 
-# Write it to disk as wave file
+# Write it to disk
 with open('output.mid', 'wb') as output:
     mf.writeFile(output)
--- src/index.py
+++ src/index.py
@@ -1,46 +1,88 @@
-from random import randrange
+from Trie import trie
+import librosa
+from mido import MidiFile
+import mido
 from midiutil.MidiFile import MIDIFile
+import numpy as np
+from random import randrange
 
-# create your MIDI object
-mf = MIDIFile(1)     # only 1 track
-track = 0   # the only track
+T = trie.Trie()
 
-time = 0    # start at the beginning
-mf.addTrackName(track, time, 'Sample Track')
-mf.addTempo(track, time, 120)
+# Track data
+data = MidiFile('test.mid')
 
-# add some notes
+# Default bpm to 200 if tempo does not exist in the midi file
+bpm = mido.tempo2bpm(data.tracks[0][0].tempo) if hasattr(data.tracks[0][0], 'tempo') else 200
 channel = 0
 volume = 100
 
-pitch = 60           # C4 (middle C)
-time = 0             # start on beat 0
-duration = 1         # 1 beat long
-mf.addNote(track, channel, pitch, time, duration, volume)
 
-pitch = 64           # E4
-time = 2             # start on beat 2
-duration = 1         # 1 beat long
-mf.addNote(track, channel, pitch, time, duration, volume)
+# Create your MIDI object
+mf = MIDIFile(1)     # only 1 track
+midi_track = 0   # the only track
 
-pitch = 67           # G4
-time = 4             # start on beat 4
-duration = 1         # 1 beat long
-mf.addNote(track, channel, pitch, time, duration, volume)
+time = 0    # start at the beginning
+mf.addTrackName(midi_track, time, "Sample Track")
+mf.addTempo(midi_track, time, bpm)
 
-# Add some random notes
-for i in range(6, 10):
-    print(i)
+starting_note = librosa.midi_to_note(data.tracks[1][1].note)
+current_note = starting_note
+
+# Insert MIDI notes into Trie data structure
+for track in data.tracks:
+    for message in track:
+        if message.type == 'note_on':
+            next_note = librosa.midi_to_note(message.note)
+            T.insert(current_note+'-'+next_note)
+            current_note = next_note
+
+def predict_next_note(note):
+    """Predict next note based on current state"""
+    count_appearance = dict((note, appereance) for note, appereance in T.search(note))
+
+    # convert apperance into probabilities
+    for key in count_appearance.keys():
+        count_appearance[key] = count_appearance[key]/len(T.search(note))
+
+    # create list of possible options for the next chord
+    options = [key.split('-')[1] for key in count_appearance.keys()]
+    
+    # create  list of probability distribution
+    probabilities = list(count_appearance.values())
+
+    # return random prediction
+    return np.random.choice(options, p=probabilities)
+
+
+def generate_sequence(note, n):
+    """Generate sequence of n length"""
+    notes = []
+    for i in range(n):
+        notes.append(predict_next_note(note))
+        note = notes[-1]
+    return notes
+
+# n equals the length of the sequence
+n = 30
+sequence = generate_sequence(starting_note, n)
+print(f'Generated sequence = {sequence}')
+
+# Current beat, start on beat 0
+current_beat = 0
+
+# Add notes to the MIDI track
+for note in sequence:
     mf.addNote(
-      track=track,
+      track=midi_track,
       channel=channel,
-      pitch=randrange(30, 80),
-      time=randrange(i, i+4),
-      duration=randrange(1, 4),
-      volume=volume
+      pitch=librosa.note_to_midi(note),
+      time=current_beat,
+      duration=randrange(1,4),
+      volume=randrange(70, 100)
     )
+    current_beat += randrange(1, 3)
 
 
-# write it to disk
+# Write it to disk
 with open('output.mid', 'wb') as output:
     mf.writeFile(output)
