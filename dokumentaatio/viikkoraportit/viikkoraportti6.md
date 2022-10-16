a0b5cbfccda8b133e2102ce22f60a62b6eb4d4a0
lapptomi <lappetelainen.tomi@gmail.com>
2022-10-16 07:20:26 +0300

    fixed probability calculations and added small command-line interface

---
+++ dokumentaatio/viikkoraportit/viikkoraportti6.md
@@ -0,0 +1,16 @@
+# Viikkoraportti 6
+
+## Tunteja käytetty
+Käytin yhteensä noin 14 tuntia viikon 6 aiheisiin.
+
+## Mitä olen tehnyt tällä viikolla?
+Mietin aluksi, kuinka saisin ohjelman todennäköisyydet toimimaan kaikilla eri MIDI-tiedostoilla. Tämän jälkeen ajattelin myös lisätä ohjelmaan toiminnon, jossa käyttäjä voi myös MIDI-tiedostojen sijasta itse antaa syötteen komentorivin kautta, jota käytettäisiin musiikin luomiseen.
+
+## Miten ohjelma on edistynyt?
+Tällä viikolla sain eri MIDI-tiedostojen todennäköisyydet korjattua eri tiloihin liittyen, joilla seuraavat nuotit haettaisiin. Toteutin myös pienen käyttäliittymän komentoriville, sekä myös toiminnon, jossa käyttäjä voi myös itse antaa nuotit komentoriviltä MIDI-tiedostojen sijasta.
+
+## Mitä opin tällä viikolla / tänään?
+-
+
+## Mitä teen seuraavaksi?
+Tavoitteena olisi alustavasti luoda lisää testejä sekä muita pieniä parannuksia, mitä kurssin kuvauksissa oli vaatimuksena. Tällä hetkellä ajattelin myös, että jos aika riittää, niin yrittäisin saada toiminnallisuuden luotua, jossa ohjelma hyödyntäisi monta eri MIDI-raitaa vain yhden sijasta musiikin luomiseen liittyen.
--- src/index.py
+++ src/index.py
@@ -1,88 +1,151 @@
 from Trie import trie
 import librosa
 from mido import MidiFile
-import mido
 from midiutil.MidiFile import MIDIFile
 import numpy as np
 from random import randrange
+import functools
 
 T = trie.Trie()
 
-# Track data
-data = MidiFile('test.mid')
-
-# Default bpm to 200 if tempo does not exist in the midi file
-bpm = mido.tempo2bpm(data.tracks[0][0].tempo) if hasattr(data.tracks[0][0], 'tempo') else 200
-channel = 0
-volume = 100
-
-
-# Create your MIDI object
-mf = MIDIFile(1)     # only 1 track
-midi_track = 0   # the only track
-
-time = 0    # start at the beginning
-mf.addTrackName(midi_track, time, "Sample Track")
-mf.addTempo(midi_track, time, bpm)
-
-starting_note = librosa.midi_to_note(data.tracks[1][1].note)
-current_note = starting_note
-
-# Insert MIDI notes into Trie data structure as bigrams
-for track in data.tracks:
-    for message in track:
-        if message.type == 'note_on':
-            next_note = librosa.midi_to_note(message.note)
-            T.insert(current_note+'-'+next_note)
-            current_note = next_note
-
 def predict_next_note(note):
     """Predict next note based on current state"""
-    count_appearance = dict((note, appereance) for note, appereance in T.search(note))
+    bigrams_with_current_note = dict((note, appereance) for note, appereance in T.search(note))
+    
+    if len(bigrams_with_current_note) == 0:
+        return None
+
+    appereance = functools.reduce(lambda a, b: a + b, bigrams_with_current_note.values())
 
     # convert apperance into probabilities
-    for key in count_appearance.keys():
-        count_appearance[key] = count_appearance[key]/len(T.search(note))
+    for bigram in bigrams_with_current_note.keys():
+        bigrams_with_current_note[bigram] = bigrams_with_current_note[bigram] / appereance
 
     # create list of possible options for the next note
-    options = [key.split('-')[1] for key in count_appearance.keys()]
+    options = [bigram.split('-')[1] for bigram in bigrams_with_current_note.keys()]
     
-    # create  list of probability distribution
-    probabilities = list(count_appearance.values())
+    # create list of probability distribution
+    probabilities = list(bigrams_with_current_note.values())
 
     # return random prediction
     return np.random.choice(options, p=probabilities)
 
-
 def generate_sequence(note, n):
     """Generate sequence of n length"""
     sequence = []
+
     for i in range(n):
-        sequence.append(predict_next_note(note))
-        note = sequence[-1]
+        next_note = predict_next_note(note)
+        # Break if there is no next note to move from the current one
+        if next_note == None:
+            break
+        else:
+            sequence.append(next_note)
+            note = sequence[-1]
     return sequence
 
-# n equals the length of the sequence
-n = 30
-sequence = generate_sequence(starting_note, n)
-print(f'Generated sequence = {sequence}')
+def write_midi_to_disk(sequence):
+    """Create MIDI-file from the generated sequence"""
 
-# Current beat, start on beat 0
-current_beat = 0
+    # Default bpm to 200 if tempo does not exist in the midi file
+    # bpm = mido.tempo2bpm(data.tracks[0][0].tempo) if hasattr(data.tracks[0][0], 'tempo') else 200
+    bpm = 200
+    channel = 0
 
-# Add notes to the MIDI track
-for note in sequence:
-    mf.addNote(
-      track=midi_track,
-      channel=channel,
-      pitch=librosa.note_to_midi(note),
-      time=current_beat,
-      duration=randrange(1,4),
-      volume=randrange(70, 100)
-    )
-    current_beat += randrange(1, 3)
+    # Create your MIDI object
+    mf = MIDIFile(1)  # only 1 track
+    midi_track = 0    # the only track
 
+    time = 0 # start at the beginning
+    mf.addTrackName(midi_track, time, "Sample Track")
+    mf.addTempo(midi_track, time, bpm)
 
-# Write it to disk
-with open('output.mid', 'wb') as output:
-    mf.writeFile(output)
+    # Current beat, start on beat 0
+    current_beat = 0
+
+    # Add notes to the MIDI track
+    for note in sequence:
+        mf.addNote(
+            track=midi_track,
+            channel=channel,
+            pitch=librosa.note_to_midi(note),
+            time=current_beat,
+            duration=randrange(1,4),
+            volume=randrange(70, 100)
+        )
+        current_beat += randrange(1, 3)
+
+    # Write it to disk
+    with open('output.mid', 'wb') as output:
+        mf.writeFile(output)
+
+def generate_from_midi(data):
+    """Generate sequence by using MIDI-file as a training data"""
+
+    starting_note = None
+    current_note = None
+
+    # Insert MIDI notes into Trie data structure as bigrams
+    for track in data.tracks:
+        for message in track:
+            if message.type == 'note_on':
+                if starting_note == None or current_note == None:
+                    starting_note = librosa.midi_to_note(message.note, unicode=False)
+                    current_note = librosa.midi_to_note(message.note, unicode=False)
+                    pass
+                else:
+                    next_note = librosa.midi_to_note(message.note, unicode=False)
+                    T.insert(f'{current_note}-{next_note}')
+                    current_note = next_note
+                
+    return generate_sequence(starting_note, 30)
+
+def generate_from_user_input():
+    """Generate sequence using the user input as a training data"""
+
+    print('\nEnter notes to generate music from, (for example C, D#, E, E#, F, etc...)')
+    print('Enter empty input to stop reading input from user. \n')
+    
+    notes = librosa.midi_to_note(list(range(60, 72)), octave=False, unicode=False)
+    sequence = []
+
+    while True:
+        note = input('Enter note: ').capitalize()
+        if not note:
+            break
+        elif note not in notes:
+            print(f'Invalid input, note must equal one of the following: {notes}')
+        elif note in notes:
+            sequence.append(note)
+            print(f'Added notes: {sequence}')
+
+    starting_note = sequence[0]
+    current_note = starting_note
+
+    # Insert MIDI notes into Trie data structure as bigrams
+    for note in sequence[1:]:
+        next_note = note
+        T.insert(f'{current_note}-{next_note}')
+        current_note = next_note
+                
+    return generate_sequence(starting_note, 30)
+
+def main():
+    print('Enter number "1" to use a MIDI-file as a training data: ')
+    print('Enter number "2" to insert notes manually from console:')
+    option = input('Choose option: ')
+    
+    if option == '1':
+        file_name = input('Enter the name of the MIDI-file: ')
+        data = MidiFile(file_name)
+        generated_sequence = generate_from_midi(data)
+        print(f'Generated sequence = {generated_sequence}')
+        write_midi_to_disk(generated_sequence)
+    elif option == '2':
+        generated_sequence = generate_from_user_input()
+        print(f'Generated sequence = {generated_sequence}')
+        write_midi_to_disk(generated_sequence)
+    else:
+        quit()
+
+main()
